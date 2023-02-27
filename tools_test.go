9b4a95095c033594fb323d865336aa1dcf0a0aeb
Joshua Folivi <joshua.folivi@gmail.com>
2023-02-27 19:04:15 +0000

    add tests

--- tools_test.go
+++ tools_test.go
@@ -30,6 +30,10 @@ var slugTests = []struct {
 	errorExpected bool
 }{
 	{name: "valid string", s: "now is the time", expected: "now-is-the-time", errorExpected: false},
+	{name: "empty string", s: "", expected: "", errorExpected: true},
+	{name: "complex string", s: "Now is the time for all GOOD men! + fish & such &^123", expected: "now-is-the-time-for-all-good-men-fish-such-123", errorExpected: false},
+	{name: "japanese string", s: "こんにちは世界", expected: "", errorExpected: true},
+	{name: "japanese string and roman characters", s: "hello world こんにちは世界", expected: "hello-world", errorExpected: false},
 }
 
 func TestTools_RandomString(t *testing.T) {
