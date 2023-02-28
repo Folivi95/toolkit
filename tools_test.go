e0a813c9707addf140b7da35378ac58e66e220f9
Joshua Folivi <joshua.folivi@gmail.com>
2023-02-28 18:34:51 +0000

    add push to remote tests

--- tools_test.go
+++ tools_test.go
@@ -16,6 +16,18 @@ import (
 	"testing"
 )
 
+type RoundTripFunc func(req *http.Request) *http.Response
+
+func (f RoundTripFunc) RoundTrip(req *http.Request) (*http.Response, error) {
+	return f(req), nil
+}
+
+func NewTestClient(fn RoundTripFunc) *http.Client {
+	return &http.Client{
+		Transport: fn,
+	}
+}
+
 var uploadTests = []struct {
 	name          string
 	allowedTypes  []string
@@ -325,5 +337,28 @@ func TestTools_ErrorJSON(t *testing.T) {
 
 	if rr.Code != http.StatusServiceUnavailable {
 		t.Errorf("wrong status code returned; expected 503, but got %d", rr.Code)
+	}
+}
+
+func TestTools_PushJSONToRemote(t *testing.T) {
+	client := NewTestClient(func(req *http.Request) *http.Response {
+		// Test Request Parameters
+		return &http.Response{
+			StatusCode: http.StatusOK,
+			Body:       io.NopCloser(bytes.NewBufferString("ok")),
+			Header:     make(http.Header),
+		}
+	})
+
+	var testTools Tools
+	var foo struct {
+		Bar string `json:"bar"`
+	}
+
+	foo.Bar = "bar"
+
+	_, _, err := testTools.PushJSONToRemote("http://example.com/some/path", foo, client)
+	if err != nil {
+		t.Error("failed to call remote URL:", err)
 	}
 }
